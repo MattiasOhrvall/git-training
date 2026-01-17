a4b9d9f633ccd8d149150e2be2273b2c70924465
rXssu <mattiasfo@hotmail.com>
2026-01-17 15:23:37 +0100

    Jag har lagt till en kritiskt metod i koden

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Tränar att använda Git");
+        branchMetod();
+    }
+
+    private static void branchMetod() {
+        System.out.println("Jag har skapat en branch som heter feature/test-branch");
     }
 }
